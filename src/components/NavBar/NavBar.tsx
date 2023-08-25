888080ed68dc1b33086827ce771d113d99a3940b
Uday Goel <udaygoel2000@gmail.com>
2023-08-25 13:44:36 +0530

    Updated navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,19 +1,59 @@
 import React from "react";
 import Logo from "../Logo/Logo";
 import styles from "./NavBar.module.css";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [menuClicked, setMenuClicked] = useState(false);
+  const toggleMenu = () => {
+    setMenuClicked(!menuClicked);
+  };
   return (
-    <div className={styles.wrapper}>
-      <Logo />
-      <div className={styles.secondWrapper}>
-        <p>Home</p>
-        <p>Attorneys</p>
-        <p>Practise Areas</p>
-        <p>About us</p>
+    <>
+      <div className={styles.wrapper}>
+        <button className={styles.wrapperMobile} onClick={() => toggleMenu()}>
+          <div className={styles.menuMobile}></div>
+          <div className={styles.menuMobile}></div>
+          <div className={styles.menuMobile}></div>
+        </button>
+        <Logo />
+        <div className={styles.secondWrapper}>
+          <p>Home</p>
+          <p>Attorneys</p>
+          <p>Practise Areas</p>
+          <p>About us</p>
+        </div>
+        <button className={styles.navBarButton}>Contact Now</button>
       </div>
-      <button className={styles.navBarButton}>Contact Now</button>
-    </div>
+      <div className={styles.dropDown}>
+        <ul>
+          <li
+            className={styles.dropDownList}
+            style={{ display: menuClicked ? "block" : "none" }}
+          >
+            <center>Home</center>
+          </li>
+          <li
+            className={styles.dropDownList}
+            style={{ display: menuClicked ? "block" : "none" }}
+          >
+            <center>Attorneys</center>
+          </li>
+          <li
+            className={styles.dropDownList}
+            style={{ display: menuClicked ? "block" : "none" }}
+          >
+            <center>Practise Areas</center>
+          </li>
+          <li
+            className={styles.dropDownList}
+            style={{ display: menuClicked ? "block" : "none" }}
+          >
+            <center>About us</center>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
