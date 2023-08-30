8926d9c7cb10df6ee5d84bf7c055dfba607ea56d
Uday Goel <udaygoel2000@gmail.com>
2023-08-30 20:28:46 +0530

    Added client carousel

--- src/components/ClientCard/ClientCard.tsx
+++ src/components/ClientCard/ClientCard.tsx
@@ -1,65 +1,30 @@
-import React from "react";
-import styles from "./ClientCard.module.css";
-import JaneCooper from "../../assets/clients/JaneCooper.png";
-import DevonLane from "../../assets/clients/DevonLane.png";
-import RobertFox from "../../assets/clients/RobertFox.png";
+// import styles from "./ClientCard.module.css";
+
+// import "swiper/css";
+// type CardData = {
+//   icon: JSX.Element;
+//   id: number;
+//   main_heading: string;
+//   sub_heading: string;
+//   content: string;
+// };
+
+// type Props = {
+//   clientData: any;
+// };
 
 const ClientCard = () => {
-  const cardDataObj = {
-    sub_heading: "Ceo of Hunt",
-    content:
-      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer",
-  };
-  const cardsData = [
-    {
-      ...cardDataObj,
-      icon: (
-        <img
-          src={JaneCooper}
-          alt="JaneCooper"
-          style={{ padding: "8% 0 3% 8%", width: "90%" }}
-        />
-      ),
-      id: 1,
-      main_heading: "Jane Cooper",
-    },
-    {
-      ...cardDataObj,
-      icon: (
-        <img
-          src={DevonLane}
-          alt="DevonLane"
-          style={{ padding: "8% 0 3% 8%", width: "90%" }}
-        />
-      ),
-      id: 2,
-      main_heading: "Devon Lane",
-    },
-    {
-      ...cardDataObj,
-      icon: (
-        <img
-          src={RobertFox}
-          alt="RobertFox"
-          style={{ padding: "8% 0 3% 8%", width: "90%" }}
-        />
-      ),
-      id: 3,
-      main_heading: "Robert Fox",
-    },
-  ];
-  return (
-    <div className={styles.wrapper}>
-      {cardsData.map((cardData) => (
-        <div className={styles.card} key={cardData.id}>
-          <div className={styles.circleFOrDP}>{cardData.icon}</div>
-          <p className={styles.mainHeading}>{cardData.main_heading}</p>
-          <p className={styles.sub_heading}>{cardData.sub_heading}</p>
-          <p className={styles.content}>{cardData.content}</p>
-        </div>
-      ))}
-    </div>
-  );
+  return;
 };
+// const ClientCard = ({ data }: Props) => {
+//   return (
+//     <div className={styles.card} key={data.id}>
+//       <div className={styles.circleFOrDP}>{data.icon}</div>
+//       <p className={styles.mainHeading}>{data.main_heading}</p>
+//       <p className={styles.sub_heading}>{data.sub_heading}</p>
+//       <p className={styles.content}>{data.content}</p>
+//     </div>
+//   );
+// };
 
 export default ClientCard;
--- src/components/ClientSection/ClientSection.tsx
+++ src/components/ClientSection/ClientSection.tsx
@@ -1,20 +1,175 @@
-import React from "react";
 import styles from "./ClientSection.module.css";
 import { ReactComponent as LeftArrow } from "../../assets/LeftArrow.svg";
 import { ReactComponent as RightArrow } from "../../assets/RightArrow.svg";
-import ClientCard from "../ClientCard/ClientCard";
+// import ClientCard from "../ClientCard/ClientCard";
+import JaneCooper from "../../assets/clients/JaneCooper.png";
+import DevonLane from "../../assets/clients/DevonLane.png";
+import RobertFox from "../../assets/clients/RobertFox.png";
+import React, { useEffect, useRef } from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+
+interface clientCardData {
+  id: number;
+  main_heading: string;
+  // icon: string;
+  icon: JSX.Element;
+  // cardDataObj: cardDataObjInt;
+  sub_heading: string;
+  content: string;
+}
+
+interface cardsDataArray {
+  // data: cardData[];
+  data: Array<clientCardData>;
+}
 
 const ClientSection = () => {
+  const swiper = useSwiper();
+  const sliderRef = useRef<any | null>(null);
+  const handleNext = (next: boolean) => {
+    console.log(sliderRef.current.swiper.slideNext());
+    if (!next) sliderRef.current.swiper.slidePrev();
+    else sliderRef.current.swiper.slideNext();
+    // console.log(next);
+  };
+
+  const Controls = ({ data }: cardsDataArray) => {
+    const swiper = useSwiper();
+    useEffect(() => {
+      swiper.slideTo(0, 5000);
+    }, [data]);
+
+    return <></>;
+  };
+
+  const cardDataObj = {
+    sub_heading: "Ceo of Hunt",
+    content:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer",
+  };
+  const cardsData = [
+    {
+      ...cardDataObj,
+      icon: (
+        <img
+          src={JaneCooper}
+          alt="JaneCooper"
+          style={{ padding: "8% 0 3% 8%", width: "90%" }}
+        />
+      ),
+      // icon: JaneCooper,
+      id: 1,
+      main_heading: "Jane Cooper",
+    },
+    {
+      ...cardDataObj,
+      icon: (
+        <img
+          src={DevonLane}
+          alt="DevonLane"
+          style={{ padding: "8% 0 3% 8%", width: "90%" }}
+        />
+      ),
+      // icon: DevonLane,
+      id: 2,
+      main_heading: "Devon Lane",
+    },
+    {
+      ...cardDataObj,
+      icon: (
+        <img
+          src={RobertFox}
+          alt="RobertFox"
+          style={{ padding: "8% 0 3% 8%", width: "90%" }}
+        />
+      ),
+      // icon: RobertFox,
+      id: 3,
+      main_heading: "Robert Fox",
+    },
+    {
+      ...cardDataObj,
+      icon: (
+        <img
+          src={JaneCooper}
+          alt="JaneCooper"
+          style={{ padding: "8% 0 3% 8%", width: "90%" }}
+        />
+      ),
+      // icon: JaneCooper,
+      id: 4,
+      main_heading: "Jane Cooper",
+    },
+    {
+      ...cardDataObj,
+      icon: (
+        <img
+          src={DevonLane}
+          alt="DevonLane"
+          style={{ padding: "8% 0 3% 8%", width: "90%" }}
+        />
+      ),
+      // icon: DevonLane,
+      id: 5,
+      main_heading: "Devon Lane",
+    },
+    {
+      ...cardDataObj,
+      icon: (
+        <img
+          src={RobertFox}
+          alt="RobertFox"
+          style={{ padding: "8% 0 3% 8%", width: "90%" }}
+        />
+      ),
+      // icon: RobertFox,
+      id: 6,
+      main_heading: "Robert Fox",
+    },
+  ];
   return (
     <div style={{}}>
       <div className={styles.wrapper}>
         <p className={styles.mainHeading}>What says our happy Clients</p>
-        <div>
-          <LeftArrow className={styles.leftArrow} />
-          <RightArrow className={styles.rightArrow} />
+        <div style={{ display: "flex", justifyContent: "center", gap: "50px" }}>
+          <LeftArrow
+            className={styles.leftArrow}
+            onClick={() => handleNext(false)}
+          />
+          <RightArrow
+            className={styles.rightArrow}
+            onClick={() => handleNext(true)}
+          />
         </div>
       </div>
-      <ClientCard />
+      <div className={styles.wrapper1}>
+        <Swiper
+          {...swiper}
+          initialSlide={0}
+          modules={[Navigation]}
+          slidesPerView={"auto"}
+          spaceBetween={145}
+          allowTouchMove={true}
+          ref={sliderRef}
+        >
+          <Controls data={cardsData} />
+          {cardsData.map((cardData: clientCardData) => (
+            <SwiperSlide key={cardData.id}>
+              <div className={styles.card} key={cardData.id}>
+                <div className={styles.circleFOrDP}>{cardData.icon}</div>
+                <p className={styles.cardMainHeading}>
+                  {cardData.main_heading}
+                </p>
+                <p className={styles.sub_heading}>{cardData.sub_heading}</p>
+                <p className={styles.content}>{cardData.content}</p>
+              </div>
+              {/* <ClientCard data={cardData} /> */}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
